1ff669bc4c83eda402f74420551b9cd4cc22490a
a22004247 <4124.g17.gfp@gmail.com>
2022-06-16 19:12:28 +0100

    Quizz

--- portfolio/templates/functions.py
+++ portfolio/templates/functions.py
@@ -37,7 +37,7 @@ def grafico_quiz(objects):
     users = list(data.keys())
     values = list(data.values())
 
-    plt.bar(users,values,color = 'green', width = 0.5)
+    plt.bar(users,values,color = 'green', width = 0.6)
 
     plt.xlabel("Jogadores")
     plt.ylabel("Pontuação")
