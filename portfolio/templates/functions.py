eb151c36003499e4518830af214a58425b5cfa06
a22004247 <4124.g17.gfp@gmail.com>
2022-06-16 20:02:03 +0100

    form_tfcs

--- portfolio/templates/functions.py
+++ portfolio/templates/functions.py
@@ -6,19 +6,15 @@ def PontuacaoQuizz(quiz):
 
     pontuacao = 0
 
-    #Qual a marca de telemoveis tem uma noticia no site ? Apple
     if(quiz.questao_1 == "Apple"):
         pontuacao += 1
 
-    #Quantas tecnologias são faladas ? 13
     if(quiz.questao_2 == 22):
         pontuacao += 1
 
-    #Laravel surgio em que ano ? 2011
     if(quiz.questao_3 == 2011):
         pontuacao += 1
 
-    #O que quer dizer o príncipio DRY do django ? Don't Repeat Yourself
     if(quiz.questao_4 == "Don't Repeat Yourself"):
         pontuacao += 1
 
